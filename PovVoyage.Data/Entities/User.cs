66cd230e6ec155632daeb35b508d2f8f0de6a62d
sukumar2267 <sukumark2267@gmail.com>
2025-08-06 15:31:27 +0530

    postgesql

--- PovVoyage.Data/Entities/User.cs
+++ PovVoyage.Data/Entities/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,8 @@ namespace PovVoyage.Data.Entities
 {
     public class User
     {
+        [Key]
+        [Column(TypeName = "uuid")]
         public Guid UserId { get; set; }
         public string Username { get; set; }
         public string FirstName { get; set; }
